7816051fe0a0ff2715f5d2ea411b8d8915cf9ef8
Piotr Kinowski <piotr.kinow@gmail.com>
2025-11-18 01:14:06 +0100

    Test parametrowy bestPerformingEmployee - faza Red

--- src/test/java/com/example/service/GradingServiceTest.java
+++ src/test/java/com/example/service/GradingServiceTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
@@ -27,7 +29,7 @@ public class GradingServiceTest {
                 "TechCorp", Position.PROGRAMISTA, 8500));
         employeeService.addEmployee(new Employee("Anna", "Nowak", "a.nowak@techcorp.pl",
                 "TechCorp", Position.MANAGER, 13000));
-        employeeService.addEmployee(new Employee("Janusz", "Kowalski", "j.kowalski@techcorp.pl",
+        employeeService.addEmployee(new Employee("Andrzej", "Kowalski", "a.kowalski@techcorp.pl",
                 "InnaFirma", Position.MANAGER, 15000));
     }
 
@@ -54,6 +56,46 @@ public class GradingServiceTest {
                 Arguments.of(Arrays.asList(3.0, 4.0, 5.0), 4.0),
                 Arguments.of(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0), 3.0),
                 Arguments.of(Arrays.asList(5.0, 5.0, 3.0, 4.0), 4.25)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideGradesWithEmail")
+    void testBestPerformingEmployee(Map<String,List<Double>> grades, String expectedResult) {
+        // Arrange
+        for (String email : grades.keySet()) {
+            List<Double> employeeGrades =  grades.get(email);
+            for (Double grade : employeeGrades) {
+                gradingService.addGrade(email, grade);
+            }
+        }
+
+        // Act
+        String result = gradingService.bestPerformingEmployee();
+
+        // Assert
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    static Stream<Arguments> provideGradesWithEmail() {
+        Map<String,List<Double>> employeesGrades1 = new HashMap<>();
+        employeesGrades1.put("j.kowalski@techcorp.pl",Arrays.asList(3.0, 4.0, 5.0));
+        employeesGrades1.put("a.nowak@techcorp.pl",Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0));
+        employeesGrades1.put("a.kowalski@techcorp.pl",Arrays.asList(5.0));
+
+        Map<String,List<Double>> employeesGrades2 = new HashMap<>();
+        employeesGrades1.put("j.kowalski@techcorp.pl",Arrays.asList(3.0, 4.0, 2.0));
+        employeesGrades1.put("a.nowak@techcorp.pl",Arrays.asList(2.0, 2.0, 3.0, 4.0, 4.0));
+        employeesGrades1.put("a.kowalski@techcorp.pl",Arrays.asList(4.0, 3.5));
+
+        Map<String,List<Double>> employeesGrades3 = new HashMap<>();
+        employeesGrades1.put("j.kowalski@techcorp.pl",Arrays.asList(3.5, 4.5, 5.0));
+        employeesGrades1.put("a.nowak@techcorp.pl",Arrays.asList(2.0, 3.5, 5.0, 5.0, 5.0));
+        employeesGrades1.put("a.kowalski@techcorp.pl",Arrays.asList(4.0));
+        return Stream.of(
+                Arguments.of(employeesGrades1, "a.kowalski@techcorp.pl"),
+                Arguments.of(employeesGrades2, "a.kowalski@techcorp.pl"),
+                Arguments.of(employeesGrades3, "j.kowalski@techcorp.pl")
         );
     }
 
